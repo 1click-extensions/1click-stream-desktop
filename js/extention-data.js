05f61e970580cf391fb9397f34b44bff3f6c422d
Itsick <any.site.n.site@gmail.com>
2018-06-21 19:38:33 +0300

    add z-index to popup

--- js/extention-data.js
+++ js/extention-data.js
@@ -1,2 +1,2 @@
-const _extension={"id":"1click-stream-desktop","video":"GR6J3lFLrj4","chromeId":"none","name":"1click Stream Desktop","short_description":"Stream your desktop online","description":"","notPublished":true,"background":"#33AF3854"}
+const _extension={"id":"1click-stream-desktop","video":"GR6J3lFLrj4","chromeId":"none","name":"1Click Stream Desktop","short_description":"Stream your desktop online","description":"","notPublished":true,"background":"#33AF38"}
 
